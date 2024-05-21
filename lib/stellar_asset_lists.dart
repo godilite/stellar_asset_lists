4dabe177b3042bd23fd2595cc3191627c3fdcb89
Godwin Asuquo <godilite@gmail.com>
2024-05-21 15:59:59 +0300

    Update description

--- lib/stellar_asset_lists.dart
+++ lib/stellar_asset_lists.dart
@@ -1,4 +1,4 @@
-///The Sellar Asset Lists package is designed to facilitate the integration of curated Stellar asset lists into Stellar applications and services. This package adheres to the Stellar Ecosystem Proposal (SEP-42) standard, providing a standardized mechanism for defining, validating, and sharing lists of Stellar assets. This helps to enhance user experiences and trust by ensuring consistency and ease of integration across the Stellar network.
+///The Stellar Asset Lists package is designed to facilitate the integration of curated Stellar asset lists into Stellar applications and services. This package adheres to the Stellar Ecosystem Proposal (SEP-42) standard, providing a standardized mechanism for defining, validating, and sharing lists of Stellar assets. This helps to enhance user experiences and trust by ensuring consistency and ease of integration across the Stellar network.
 library;
 
 export 'src/stellar_asset_lists_base.dart';
